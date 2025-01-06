f8c6d6170dddd5486eedcd0ea0ba0f2605d43c22
Takahito Urai <uraitakahito@gmail.com>
2025-01-06 21:03:33 +0900

    Add forEach implementation and tests; rename prime function for consistency

--- test/concurrent.test.ts
+++ test/concurrent.test.ts
@@ -6,35 +6,35 @@ import * as assert from 'node:assert';
 const sleep = (t: number) => new Promise((resolve) => setTimeout(resolve, t));
 
 beforeAll(async () => {
-  await sleep(1000);
+  await sleep(100);
   console.log('beforeAll() complete.');
 });
 afterAll(async () => {
-  await sleep(1000);
+  await sleep(100);
   console.log('afterAll() complete.');
 });
 beforeEach(async () => {
-  await sleep(1000);
+  await sleep(100);
   console.log('beforeEach() complete.');
 });
 afterEach(async () => {
-  await sleep(1000);
+  await sleep(100);
   console.log('afterEach() complete.');
 });
 
 describe('concurrent suite', () => {
   it.concurrent('a', async () => {
-    await sleep(1000);
+    await sleep(100);
     assert.equal(1, 1);
     console.log('test a complete.');
   });
   it.concurrent('b', async () => {
-    await sleep(1000);
+    await sleep(100);
     assert.equal(2, 2);
     console.log('test b complete.');
   });
   it.concurrent('c', async () => {
-    await sleep(1000);
+    await sleep(100);
     assert.equal(3, 3);
     console.log('test c complete.');
   });
